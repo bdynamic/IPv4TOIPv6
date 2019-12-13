6eb7a44f61d03629c5070d63ddd94615525d91b9
Birk Bremer <birk.bremer@5pm.de>
2019-12-13 09:03:11 +0100

    First commit

---
+++ haproxy_genconfig.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+TEMPLATEPATH="./templates"
+ENTRYSPATH="./entries"
+DSTCONFIG="./haproxy.cfg.tmp"
+
+
+#generate absolut paths
+TEMPLATEPATH="$(realpath $TEMPLATEPATH)"
+ENTRYSPATH="$(realpath $ENTRYSPATH)"
+DSTCONFIG="$(realpath $DSTCONFIG)"
+
+
+echo "Writing config to $DSTCONFIG"
+cat "$TEMPLATEPATH/global.template" >"$DSTCONFIG"
+cat "$TEMPLATEPATH/defaults.template" >>"$DSTCONFIG"
+cat "$TEMPLATEPATH/frontend_start.template" >>"$DSTCONFIG"
+
+
+
+## Generation of frontend entries
+for VALUE in $(ls -1 $ENTRYSPATH/*.entry)
+do 
+   #echo "Generting frontend entry for $VALUE"
+   source "$VALUE"
+   echo "Generating frontend entry for: $name"
+   echo "   ### $name ">>"$DSTCONFIG"
+   if [ $unencrypted == true ]; then
+   	 #echo "Create unencrypted entry"
+   	 cat "$TEMPLATEPATH/frontend_entry.template" | sed "s/<name>/$name/g" | sed "s/<url>/$url/g" >>"$DSTCONFIG"
+   	 echo "" >>"$DSTCONFIG"
+   fi
+
+  if [ $ssl == true ]; then
+   	 #echo "Create encrypted entry"
+   	 cat "$TEMPLATEPATH/frontend_ssl_entry.template" | sed "s/<name>/$name/g" | sed "s/<url>/$url/g" >>"$DSTCONFIG"
+   	 echo "" >>"$DSTCONFIG"
+  fi
+
+  echo ""
+  echo "" >>"$DSTCONFIG"
+done
+
+
+
+## Generation of backend entries
+for VALUE in $(ls -1 $ENTRYSPATH/*.entry)
+do 
+   #echo "Generting backend entry for $VALUE"
+   source "$VALUE"
+   echo "Generating backend entry for: $name"
+
+   if [ $unencrypted == true ]; then
+   	 echo "Create unencrypted entry"
+   	 cat "$TEMPLATEPATH/backend_entry.template" | sed "s/<name>/$name/g" | sed "s/<ipv6>/$ipv6/g" >>"$DSTCONFIG"
+   	 echo "" >>"$DSTCONFIG"
+   fi
+
+  if [ $ssl == true ]; then
+   	 echo "Create encrypted entry"
+   	 cat "$TEMPLATEPATH/backend_ssl_entry.template" | sed "s/<name>/$name/g" | sed "s/<ipv6>/$ipv6/g" >>"$DSTCONFIG"
+   	 echo "" >>"$DSTCONFIG"
+  fi
+
+  echo ""
+  echo "" >>"$DSTCONFIG"
+done
+
+
+exit 0
